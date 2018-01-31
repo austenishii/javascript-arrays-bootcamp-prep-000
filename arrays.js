aec5efa46de7a101c3a9e8aaf100763c2270d191
Austen Ishii <austenishii@gmail.com>
2018-01-31 15:17:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(chocolateBars, foo) {["foo", ...chocolateBars]; return chocolateBars}
+function addElementToBeginningOfArray(chocolateBars, foo) {var newChocolateBars =["foo", ...chocolateBars]; return newChocolateBars}
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, foo) {chocolateBars.unshift("foo"); return chocolateBars}
+
