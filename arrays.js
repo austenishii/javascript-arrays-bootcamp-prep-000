a4ef771132c1db7690a0a3189c3a147887dc5514
Austen Ishii <austenishii@gmail.com>
2018-01-31 16:06:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,8 +12,8 @@ function accessElementInArray (chocolateBars, two) {return chocolateBars[2]}
 
 function destructivelyRemoveElementFromBeginningOfArray (chocolateBars) {chocolateBars.shift(); return chocolateBars}
 
-function removeElementFromBeginningOfArray (chocolateBars) {var newChocolateBars = [chocolateBars.slice(1)]; return chocolateBars}
+function removeElementFromBeginningOfArray (chocolateBars) {var newChocolateBars = [chocolateBars.slice(1)]; return newChocolateBars}
 
 function destructivelyRemoveElementFromEndOfArray (chocolateBars) {chocolateBars.pop(); return chocolateBars}
 
-function removeElementFromEndOfArray (chocolateBars) {chocolateBars.slice(0,chocolateBars.length - 1); return chocolateBars}
+function removeElementFromEndOfArray (chocolateBars) {var newChocolateBars = [chocolateBars.slice(0,chocolateBars.length - 1)]; return newChocolateBars}
